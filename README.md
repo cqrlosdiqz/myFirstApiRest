0e80b273ccc23e6be867522fe4634169acd9d1f1
cqrlosdiqz <carlosdf75@gmail.com>
2021-10-04 20:16:41 +0200

    completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# myFirstApiRest
+5. Mi primera API REST
+
+Se pide crear los siguientes endpoints y testearlos con Postman:
+
+- GET /characters: Devuelve todos los personajes (array de objetos JSON)
+- GET /character?name=Fry: Devuelve los datos del personaje recibido por parámetro
+- GET /charactersByProfile?profile=Capitana: Devuelve los datos de los personajes con el perfil pasado por parámetro
+- POST /newCharacter: Recibe datos de un nuevo personaje y los guarda
+- PUT /updateCharacter?id=1: Recibe datos de un personaje preguardado, seleccionado por su id, y los actualiza
+- DELETE /deleteCharacter?id=1: Elimina los datos de un personaje preguardado, seleccionado por su id
+
+Nota: Los datos se guardarán en memoria del servidor, no en fichero ni base de datos.
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const app = express();
+const { characters } = require('./characters.js');
+
+let myCharacters = characters;
+
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/characters', (req, res) => {
+  res.send(myCharacters);
+});
+
+app.get('/character', (req, res) => {
+  const { name } = req.query;
+  res.send(myCharacters.find((character) => character.username === name));
+});
+
+app.get('/charactersByProfile', (req, res) => {
+  const { profile } = req.query;
+  res.send(myCharacters.find((character) => character.role === profile));
+});
+
+app.post('/newCharacter', (req, res) => {
+  const { id, username, role } = req.body;
+  myCharacters.push({ id: parseInt(id), username, role });
+  res.send(myCharacters);
+});
+
+app.put('/updateCharacter', (req, res) => {
+  const { id } = req.query;
+  const { username, role } = req.body;
+  myCharacters = characters.map((character) => {
+    if (character.id === parseInt(id)) {
+      character.role = role;
+      character.username = username;
+    }
+    return character;
+  });
+
+  res.send(myCharacters);
+});
+
+app.delete('/deleteCharacter', (req, res) => {
+  const { id } = req.query;
+  myCharacters = characters.filter(
+    (character) => character.id !== parseInt(id)
+  );
+  res.send(myCharacters);
+});
+
+app.use('*', (req, res) => {
+  res.send('<h1>NOT FOUND</h1>');
+});
+
+app.listen(port, () => {
+  console.log('listening on port ' + port);
+});
